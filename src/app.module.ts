746c757216583597b337233f825a370a3928c4ee
Fernando Garzon <of.garzon2662@uniandes.edu.co>
2024-10-29 15:05:48 -0700

    Club-Socio Modules added and need changing

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,32 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SocioModule } from './socio/socio.module';
+import { ClubModule } from './club/club.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SocioEntity } from './socio/socio.entity';
+import { ClubEntity } from './club/club.entity';
+import { ClubSocioModule } from './club-socio/club-socio.module';
 
 @Module({
-  imports: [],
+  imports: [SocioModule, ClubModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'clubes',
+      entities: [
+        SocioEntity,
+        ClubEntity
+      ],
+      dropSchema: true,
+      synchronize: true,
+      keepConnectionAlive: true
+    }),
+    ClubSocioModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
