38f5c8b24a43cbf831dcb02b7b74b07287b461da
Fernando Garzon <of.garzon2662@uniandes.edu.co>
2024-11-01 15:51:40 -0700

    Testing of QG

--- src/shared/validators/is-past-date.validators.ts
+++ src/shared/validators/is-past-date.validators.ts
@@ -1,17 +1,18 @@
 import {
-    ValidatorConstraint,
-    ValidatorConstraintInterface,
-    ValidationArguments,
-  } from 'class-validator';
-  
-  @ValidatorConstraint({ name: 'isPastDate', async: false })
-  export class IsPastDateConstraint implements ValidatorConstraintInterface {
-    validate(date: Date, args: ValidationArguments) {
-      return date < new Date();
-    }
-  
-    defaultMessage(args: ValidationArguments) {
-      const fieldBeingValidated: string = args.property;
-      return `La fecha de ${fieldBeingValidated} debe ser en el pasado.`;
-    }
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+  ValidationArguments,
+} from 'class-validator';
+
+@ValidatorConstraint({ name: 'isPastDate', async: false })
+export class IsPastDateConstraint implements ValidatorConstraintInterface {
+  validate(date: Date, args: ValidationArguments) {
+    console.log(args);
+    return date < new Date();
   }
+
+  defaultMessage(args: ValidationArguments) {
+    const fieldBeingValidated: string = args.property;
+    return `La fecha de ${fieldBeingValidated} debe ser en el pasado.`;
+  }
+}
