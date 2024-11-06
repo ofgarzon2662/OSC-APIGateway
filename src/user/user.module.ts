4d7a06aa26bb44af542b8880785d9e274694b381
Fernando Garzon <of.garzon2662@uniandes.edu.co>
2024-11-06 11:54:56 -0800

    Conexion with local postgres db working

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,9 +3,14 @@ import { UserService } from './user.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './user.entity';
 import { UserController } from './user.controller';
+import { OrganizationEntity } from '../organization/organization.entity';
+import { OrganizationModule } from '../organization/organization.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity])],
+  imports: [
+    TypeOrmModule.forFeature([UserEntity, OrganizationEntity]), // Ensure both entities are available
+    OrganizationModule, // Import OrganizationModule if needed
+  ],
   providers: [UserService],
   controllers: [UserController],
 })
